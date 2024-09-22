2f7236efc1492be41e381d4be2b1f794116d9d34
Jeevan Cheema <jeevancheema@icloud.com>
2024-09-22 16:35:16 +0100

    Updated the website

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+window.addEventListener('scroll', function() {
+    var element = this.document.getElementById('hero-content');
+    var position = element.getBoundingClientRect().top;
+    var windowHeight = this.window.innerHeight;
+
+    if (position < windowHeight - 200 && position > 0) {
+        element.classList.add('show');
+    } else {
+        element.classList.remove('show');
+    }
+});
